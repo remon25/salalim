a5f1deda318305a4b99897c37350ed7097dbdf30
remon25 <70450564+remon25@users.noreply.github.com>
2024-06-10 21:18:22 +0300

    major edits

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,55 @@ function closeNav() {
 
 closeNav();
 
+
+
+function setupScrollButton(button, targetElement) {
+
+    if(button.length > 0){
+        button.forEach((btn) => {
+            btn.addEventListener('click', function() {
+                // Scroll the target element into view
+                targetElement.scrollIntoView({
+                  behavior: 'smooth', // Smooth scroll
+                  block: 'start'      // Align to the top of the viewport
+                });
+              });
+        })
+    }
+    // Add a click event listener to the button
+    button.addEventListener('click', function() {
+      // Scroll the target element into view
+      targetElement.scrollIntoView({
+        behavior: 'smooth', // Smooth scroll
+        block: 'start'      // Align to the top of the viewport
+      });
+    });
+  }
+  
+  // Usage example:
+  // Assuming you have a button with id 'myButton' and a target element with id 'myTargetElement'
+  document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelectorAll('.to-about');
+    const targetElement = document.getElementById('about-container');
+  
+    setupScrollButton(button, targetElement);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelectorAll('.to-contact');
+    const targetElement = document.getElementById('contact');
+  
+    setupScrollButton(button, targetElement);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelectorAll('.to-offices');
+    const targetElement = document.getElementById('sale-container');
+  
+    setupScrollButton(button, targetElement);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.to-us');
+    const targetElement = document.getElementById('us-container');
+  
+    setupScrollButton(button, targetElement);
+  });
+  
